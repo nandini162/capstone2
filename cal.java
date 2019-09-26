a8816f462a06489880617ff8daa2a50a0a0fcb19
Nandini Goyal <41396133+nandini162@users.noreply.github.com>
2019-09-26 11:33:51 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -12,16 +12,16 @@ public class cal{
 		y=Integer.parseInt(args[1]);
 		System.out.println("Menu:\n 1) Addition\n 2)Subtract\n 3) multiply\n 4)Divide\n");
 		System.out.println("Select choice from menu(1/2/3/4):"+args[2]);
-		//n=sc.nextInt();
+		n=Integer.parseInt(args[2]);
 		switch(n)
 		{
-			case 1: ans=add.add(args[0],args[1]);
+			case 1: ans=add.add(x,y);
 			break;
-			case 2: ans=sub.sub(args[0],args[1]);
+			case 2: ans=sub.sub(x,y);
 			break;
-			case 3: ans=mutiply.multiply(args[0],args[1]);
+			case 3: ans=mutiply.multiply(x,y);
 			break;
-			case 4: ans=divide.divide(args[0],args[1]);
+			case 4: ans=divide.divide(x,y);
 			break;
 		}
 		System.out.println("Answer:"+ans);
