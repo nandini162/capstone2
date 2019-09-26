ed93a545361b7d30e3c49e1f0416eb17140aff14
Nandini Goyal <41396133+nandini162@users.noreply.github.com>
2019-09-26 11:27:42 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -6,21 +6,21 @@ public class cal{
 		int x,y,n,ans=0;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the value1:"+args[0]);
-		x=sc.nextInt();
+		//x=sc.nextInt();
 		System.out.println("Enter the value2:"+args[1]);
-		y=sc.nextInt();
+		//y=sc.nextInt();
 		System.out.println("Menu:\n 1) Addition\n 2)Subtract\n 3) multiply\n 4)Divide\n");
 		System.out.println("Select choice from menu(1/2/3/4):"+args[2]);
-		n=sc.nextInt();
+		//n=sc.nextInt();
 		switch(n)
 		{
-			case 1: ans=add.add(x,y);
+			case 1: ans=add.add(arg[0],arg[1]);
 			break;
-			case 2: ans=sub.sub(x,y);
+			case 2: ans=sub.sub(arg[0],arg[1]);
 			break;
-			case 3: ans=mutiply.multiply(x,y);
+			case 3: ans=mutiply.multiply(arg[0],arg[1]);
 			break;
-			case 4: ans=divide.divide(x,y);
+			case 4: ans=divide.divide(arg[0],arg[1]);
 			break;
 		}
 		System.out.println("Answer:"+ans);
